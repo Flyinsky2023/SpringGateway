9f02bc7c0fe3a5594e6011eb887e4de9e90360fe
王吉颖 <155232031+Flyinsky2023@users.noreply.github.com>
2025-12-13 16:33:55 +0800

    feat: 添加Spring Boot主启动类

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 主启动类
+ */
+@SpringBootApplication
+public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+}
